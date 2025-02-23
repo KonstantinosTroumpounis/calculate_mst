861333a329ee480a53cffc38fb8fd5788fc988e5
KonstantinosTroumpounis <randomtruebee@gmail.com>
2025-02-24 00:14:32 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calculate_mst/',
+  base: '/calculate_mst/',  // Important for assets to load correctly
 })
