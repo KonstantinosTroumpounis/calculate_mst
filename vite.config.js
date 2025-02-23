a0a864a46ed03313dccb29bcc5ddb545aebf6a47
KonstantinosTroumpounis <randomtruebee@gmail.com>
2025-02-24 01:04:03 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calculate_mst/',
-})
+  base: '/calculate_mst/',  // The base path for your GitHub Pages deployment
+});
