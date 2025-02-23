b73b208ec753295ff4ec513f994d640bc4082255
KonstantinosTroumpounis <randomtruebee@gmail.com>
2025-02-24 00:39:28 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useState, useEffect } from 'react'
+import { Layout, Row, Col } from 'antd'
+import GraphTheory from './js/GraphTheory';
+import GraphCustomize from './js/GraphCustomize';
+
+function App() {
+    const [addNode, setAddNode] = useState(false)
+    const [buildOwnGraphPressed, setbuildOwnGraphPressed] = useState(false)
+    const [randomGraph, setRandomGraph] = useState({
+        numberOfNodes: undefined,
+        numberOfVertices: undefined,
+        rangeOfWeights: undefined
+    })
+    const [edgesAndWeights, setEdgesAndWeights] = useState({
+        fromNode: undefined,
+        untilNode: undefined,
+        weightsNode: undefined
+    })
+    const [starterPrim, setStarterPrim] = useState({
+        startingNode: undefined,
+        options: undefined
+    })
+    const [kruskalAlg, setKruskalAlg] = useState({
+        options: undefined
+    })
+    const { Header, Content, Sider, Footer } = Layout;
+
+    const addNodePressed = () => {
+        setAddNode(!addNode)
+    }
+
+    const insertEdgesWeightsPressed = () => {
+        setbuildOwnGraphPressed(!buildOwnGraphPressed)
+    }
+
+    const randomGraphCustomize = (data) => {
+        setRandomGraph({
+            numberOfNodes: data.nodes,
+            numberOfVertices: data.probEdges,
+            rangeOfWeights: data.range
+        })
+    }
+
+    const buildGraphCustomize = (data) => {
+        setEdgesAndWeights({
+            fromNode: data.fromNode,
+            untilNode: data.untilNode,
+            weightsNode: data.weights
+        })
+    }
+
+    const primStartingPoint = (data) => {
+        setStarterPrim({
+            startingNode: data.startingNode,
+            option: data.primCustomize
+        })
+    }
+
+    const kruskalConfigurations = (data) => {
+        setKruskalAlg({
+            option: data.kruskalCustomize
+        })
+    }
+
+    const cleanPrimStatesAfterTerminate = () => {
+        setStarterPrim({
+            startingNode: undefined,
+            options: undefined
+        })
+    }
+
+    const cleanKruskalStatesAfterTerminate = () => {
+        setKruskalAlg({
+            options: undefined
+        })
+    }
+    
+
+    return (
+        <Layout>
+            <Header style={{ display: 'flex', alignItems: 'center' }}>
+                <span style={{color: 'white'}}>Minimun Spanning Tree</span>
+            </Header>
+            <Content style={{ padding: '0 48px', background: '#F5F5F5' }}>
+                <Layout
+                    style={{ padding: '24px 0' }}
+                >
+                    <Row gutter={[16, 16]}>
+                        <Col xs={24} sm={24} md={24} lg={8} xl={6}>
+                            <Sider style={{ background: '#F5F5F5' }} width={400}>
+                                <GraphCustomize 
+                                    addNodePressed={addNodePressed}
+                                    insertEdgesWeightsPressed={insertEdgesWeightsPressed}
+                                    randomGraphCustomize={randomGraphCustomize}
+                                    buildGraphCustomize={buildGraphCustomize}
+                                    primStartingPoint={primStartingPoint}
+                                    kruskalConfigurations={kruskalConfigurations}
+                                    cleanPrimStatesAfterTerminate = {cleanPrimStatesAfterTerminate}
+                                    cleanKruskalStatesAfterTerminate={cleanKruskalStatesAfterTerminate}
+                                />
+                            </Sider>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={16} xl={18}>
+                            <Content>
+                                <GraphTheory
+                                    nodeAdded={addNode}
+                                    addNodePressed={addNodePressed}
+                                    insertEdgesWeightsPressed={insertEdgesWeightsPressed}
+                                    buildOwnGraphPressed={buildOwnGraphPressed}
+                                    randomGraph={randomGraph}
+                                    edgesAndWeights={edgesAndWeights}
+                                    primStarting={starterPrim}
+                                    kruskalConfigurations={kruskalAlg}
+                                    cleanPrimStatesAfterTerminate={cleanPrimStatesAfterTerminate}
+                                />
+                            </Content>
+                        </Col>
+                    </Row>
+                </Layout>
+            </Content>
+            <Footer style={{ textAlign: 'center', background: '#F5F5F5' }}>
+                TrueBee ©{new Date().getFullYear()} Created by TrueBee team
+            </Footer>
+        </Layout>
+    )
+}
+
+export default App
